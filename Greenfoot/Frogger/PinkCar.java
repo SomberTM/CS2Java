ccfe5d3d5883367bfbfce3cb5e159632e0aa320d
SomberTM <50004094+SomberTM@users.noreply.github.com>
2020-03-02 15:46:52 -0500

    Create PinkCar.java

---
+++ Greenfoot/Frogger/PinkCar.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class PinkCar extends Utilities
+{
+    private int speed = 1;
+        
+    public PinkCar()
+    {
+        this.getImage().scale(42,35);
+    }
+    
+    public void act() 
+    {
+        MyWorld world = (MyWorld) getWorld();
+        setLocation(getX() - speed, getY());
+        if ( !world.debug ) {
+            checkCollision();
+            deleteAtEdge(this);
+        }
+    }    
+    
+    private void checkCollision()
+    {
+        Frog frog = (Frog) getOneIntersectingObject(Frog.class);
+        if ( frog != null ) {
+            getWorld().showText("Which Killer: Pink Car", 250,300); 
+            getWorld().removeObject(frog);
+        }         
+    }
+}
